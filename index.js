269df47fc26d452326256e466ab3a3152118b76b
Eamon Sheil <Eamondsheil@gmail.com>
2022-02-28 17:10:15 -0500

    commit tree 18b4c76943f5c3d3dcf87fc3ed9a897541bff82f

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here!
+const body = document.querySelector('body');
+
+const main = document.getElementById('main');
+console.log(main)
+main.remove();
+
+const newHeader = document.createElement('h1')
+newHeader.setAttribute('id', 'victory');
+newHeader.textContent = "Eamon is the champion";
+
+
+body.append(newHeader);
